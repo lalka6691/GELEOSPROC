c346bdbe5e650a6d0e6f50c7b94567c31fc54608
Pavel <pavel_dmitriev2004@mail.ru>
2023-10-23 09:58:08 +0300

    main 2

--- geleoproc/cpucomp/views.py
+++ geleoproc/cpucomp/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'test.html')
+    return render(request, 'index.html')
 
 def addcpu(request):
     return HttpResponse("<h1>Добавление процессоров</h1>")
--- geleoproc/geleoproc/settings.py
+++ geleoproc/geleoproc/settings.py
@@ -127,3 +127,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.core.cache import cache
+cache.clear()
